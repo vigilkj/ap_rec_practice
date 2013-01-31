ee1a3a502fb504ce570d2abb57b505e07ad6f39d
vigilkj <vigilkj@s.dcsdk12.org>
2013-01-31 08:50:18 -0700

    first commit

---
+++ palindrome.java
@@ -0,0 +1,54 @@
+	/**
+	 * palindrome
+	 *
+	 * 		returns if a string is a palindrome
+	 *
+	 * Kyle Vigil
+	 */
+class palindrome {
+
+	/**
+	 * main
+	 *
+	 * 		prints out if the string is a boolean
+	 *
+	 *@param a string
+	 */
+	public static void main(String[] args) {
+		if(iterativePalindrome(args[0]) == true){
+			System.out.println("The word " + args[0] + " is a palendrome");
+		} else {
+			System.out.println("The word " + args[0] + " is not a palendrome");
+		}
+	}
+
+	/**
+	 * iterativePalindrome
+	 *
+	 * 		returns if the string is a palindrome
+	 *
+	 * @param a string
+	 * @return true or false if it is a palindrome
+	 */
+	public static boolean iterativePalindrome(String pal){
+		for(int i=1; i<pal.length(); i++){
+			if(pal.charAt(i-1) != pal.charAt(pal.length() - i)){
+				return false;
+			}
+		}
+		return true;
+	}
+
+
+	/**
+	 * recursivePalindrome
+	 *
+	 * 		returns if the string is a palindrome
+	 *
+	 * @param a string
+	 * @return true or false if it is a palindrome
+	 */
+	public static boolean recursivePalindrome(String pal){
+		return false;
+	}
+}
